19db9c40decf447a017c0ec5bd25a98a0af962d0
Zoebumba <m199956@gmail.com>
2019-11-21 02:33:34 +0300

    Фуллскрин

--- LUKOIL/app/src/main/java/com/example/lukoil/MainActivity.java
+++ LUKOIL/app/src/main/java/com/example/lukoil/MainActivity.java
@@ -38,6 +38,7 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         initUI();
 
         _switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -133,5 +134,19 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
 
         rescan.setVisibility(View.GONE);
         btn.setClickable(false);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
     }
 }
--- LUKOIL/app/src/main/java/com/example/lukoil/ResultActivity.java
+++ LUKOIL/app/src/main/java/com/example/lukoil/ResultActivity.java
@@ -19,10 +19,12 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         initUI();
 
         txt.setText(MainActivity.info);
-        img.setImageResource(R.drawable.logo_lukoil);
+        img.setImageResource(R.drawable.test_pic);
     }
 
     public void click(View v) {
@@ -35,5 +37,19 @@ public class ResultActivity extends AppCompatActivity {
     private void initUI() {
         txt = (TextView)findViewById(R.id.textView2);
         img = (ImageView)findViewById(R.id.imageView);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
     }
 }
